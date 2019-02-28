4fd4189f1eaf147ed905697a784aba6060a28dc9
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-02-28 19:50:29 +0100

    Planet5

--- BlackThornJam/Assets/Script/NavMap/MoveCamera.cs
+++ BlackThornJam/Assets/Script/NavMap/MoveCamera.cs
@@ -13,7 +13,7 @@ public class MoveCamera : MonoBehaviour
 
     void Update()
     {
-        if (transform.position.x > -11&&Input.GetKey(KeyCode.LeftArrow))
+        if (transform.position.x > -11&&(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))
         {
             transform.position = new Vector3(transform.position.x - force, transform.position.y,transform.position.z);
         }
@@ -21,7 +21,7 @@ public class MoveCamera : MonoBehaviour
         {
             transform.position = new Vector3(-11, transform.position.y,transform.position.z);
         }
-        if (transform.position.x < 431 && Input.GetKey(KeyCode.RightArrow))
+        if (transform.position.x < 431 && (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))
         {
             transform.position = new Vector3(transform.position.x + force, transform.position.y, transform.position.z);
         }
