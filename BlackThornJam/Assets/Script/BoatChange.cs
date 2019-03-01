c3235729f1041e3a754a80f9f1433e88a77487aa
Leon <46721103+LeonPyramid@users.noreply.github.com>
2019-03-01 15:46:24 +0100

    encore des ajouts

---
+++ BlackThornJam/Assets/Script/BoatChange.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatChange : MonoBehaviour
+{
+    public Sprite well;
+    public Sprite bad;
+    private bool State;
+    private bool temp;
+    private SpriteRenderer spriteR;
+    private GameObject[] buildable;
+    public int buildId;
+    private Sprite get;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildable = GameObject.FindGameObjectsWithTag("Buildable");
+        spriteR = GetComponent<SpriteRenderer>();
+        State = false;
+        temp = false;
+        get = bad;
+        foreach(GameObject build in buildable)
+        {
+            if (build.GetComponent<BuildInfo>().id == buildId)
+            {
+                State = true;
+                get = well;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        State = false;
+        get = bad;
+        foreach (GameObject build in buildable)
+        {
+            if (build.GetComponent<BuildInfo>().id == buildId)
+            {
+                State = true;
+                get = null;
+            }
+        }
+        if(temp!= State)
+        {
+            StartCoroutine(ChangeSprite(get));
+            temp = State;
+        }
+        StartCoroutine(ChangeSprite(get));
+    }
+    public IEnumerator ChangeSprite(Sprite sprite)
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+        spriteR.sprite = sprite;
+
+    }
+}
---
+++ BlackThornJam/Assets/Script/ChangeState1.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeState1 : MonoBehaviour
+{
+    public Sprite well;
+    public Sprite bad;
+    private bool State;
+    private GlobalVar global;
+    private SpriteRenderer spriteR;
+    public enum factor { argent, ecologie };
+    public factor fact;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteR = GetComponent<SpriteRenderer>();
+        global = FindObjectOfType<GlobalVar>();
+        State = true;
+        spriteR.sprite = well;
+
+        if (global.ecologieBasic < 4 && fact == factor.ecologie)
+        {
+            State = false;
+            spriteR.sprite = bad;
+        }
+        if (global.argentBasic < 4 && fact == factor.argent)
+        {
+            State = false;
+            spriteR.sprite = bad;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fact == factor.ecologie)
+        {
+            if (global.ecologieg < 4 && State && global.ecologieg != 0)
+            {
+                StartCoroutine(ChangeSprite(bad));
+                State = false;
+            }
+            else if (!State && global.ecologieg > 3 && global.ecologieg != 0)
+            {
+                StartCoroutine(ChangeSprite(well));
+                State = true;
+            }
+        }
+        else if (fact == factor.argent)
+        {
+            if (global.argentg < 4 && State && global.argentg != 0)
+            {
+                StartCoroutine(ChangeSprite(bad));
+                State = false;
+            }
+            else if (!State && global.argentg > 3 && global.argentg != 0)
+            {
+                StartCoroutine(ChangeSprite(well));
+                State = true;
+            }
+        }
+
+    }
+    public IEnumerator ChangeSprite(Sprite sprite)
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+        spriteR.sprite = sprite;
+
+    }
+}
